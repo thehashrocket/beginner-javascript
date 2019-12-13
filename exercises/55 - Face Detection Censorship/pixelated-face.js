aec4ce56ca0dd5d3b74bc003c73e05978be52fca
Jason Shultz <jasshultz@gmail.com>
2019-12-12 21:28:49 -0700

    finished exercise 55

--- exercises/55 - Face Detection Censorship/pixelated-face.js
+++ exercises/55 - Face Detection Censorship/pixelated-face.js
@@ -1,6 +1,7 @@
 const video = document.querySelector('.webcam');
 const canvas = document.querySelector('.video');
 const ctx = canvas.getContext('2d');
+
 const faceCanvas = document.querySelector('.face');
 const faceCtx = faceCanvas.getContext('2d');
 const faceDetector = new window.FaceDetector({ fastMode: true });
@@ -8,8 +9,19 @@ const optionsInputs = document.querySelectorAll(
   '.controls input[type="range"]'
 );
 
-// write a function that will populate a users video.
+const options = {
+  SIZE: 10,
+  SCALE: 1.1,
+};
 
+function handleOption(event) {
+  const { value, name } = event.currentTarget;
+  options[name] = parseFloat(value);
+}
+
+optionsInputs.forEach(input => input.addEventListener('input', handleOption));
+
+// write a function that will populate a users video.
 async function populateVideo() {
   const stream = await navigator.mediaDevices.getUserMedia({
     video: { width: 1280, height: 720 },
@@ -23,12 +35,50 @@ async function populateVideo() {
 }
 function drawFace(face) {
   const { width, height, top, left } = face.boundingBox;
-  console.log({ width, height, top, left });
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeStyle = '#ffc600';
+  ctx.lineWidth = 2;
+  ctx.strokeRect(left, top, width, height);
+}
+
+function censor({ boundingBox: face }) {
+  faceCtx.imageSmoothingEnabled = false;
+  faceCtx.clearRect(0, 0, faceCanvas.width, faceCanvas.height);
+  // draw the small face
+  faceCtx.drawImage(
+    // 5 source args
+    video, // where does the source come from?
+    face.x, // where do we start the source pull from?
+    face.y,
+    face.width,
+    face.height,
+    // 4 draw args
+    face.x, // where should we start drawing the x and y?
+    face.y,
+    options.SIZE,
+    options.SIZE
+  );
+  // take that face back out and draw it back normal size.
+  const width = face.width * options.SCALE;
+  const height = face.width * options.SCALE;
+  faceCtx.drawImage(
+    faceCanvas, // Source
+    face.x,
+    face.y,
+    options.SIZE,
+    options.SIZE,
+    // Drawing Args,
+    face.x - (width - face.width) / 2,
+    face.y - (height - face.height) / 2,
+    width,
+    height
+  );
 }
 
 async function detect() {
   const faces = await faceDetector.detect(video);
   faces.forEach(drawFace);
+  faces.forEach(censor);
   // ask the browser when the next animation frame is, and tell it to run detect for us.
   requestAnimationFrame(detect);
 }
